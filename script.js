5f6a2e346ca818c77ce9f9b865edd18ac9a7ff6a
brunnarayssa <brunnarayssa@hotmail.es>
2023-08-13 16:33:26 +0200

    ajustes no codigo

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
-
-function calcularCuotas(montoTotal, cantidadCuotas) {
+function calcularValorCuota(montoTotal, cantidadCuotas) {
   if (isNaN(montoTotal) || isNaN(cantidadCuotas) || montoTotal <= 0 || cantidadCuotas <= 0) {
     return 'Por favor, ingrese valores válidos para el monto total y la cantidad de cuotas.';
   }
 
-  const valorCuota = montoTotal / cantidadCuotas;
-  return `Valor de cada cuota: $${valorCuota.toFixed(2)}`;
+  return montoTotal / cantidadCuotas;
 }
 
-// Ejemplo de uso
-const monto = parseFloat(prompt('Ingrese el monto total:'));
-const cuotas = parseInt(prompt('Ingrese la cantidad de cuotas:'));
+function mostrarResultados(cuotas) {
+  let resultados = '';
+  for (let i = 0; i < cuotas.length; i++) {
+    resultados += `Mes ${i + 1}: Valor de cuota: $${cuotas[i].toFixed(2)}\n`;
+  }
+  alert(resultados);
+}
 
-const resultado = calcularCuotas(monto, cuotas);
-console.log(resultado);
+function calcularCuotas() {
+  const montoTotalInput = prompt('Ingrese el monto total:');
+  const cantidadCuotasInput = prompt('Ingrese la cantidad de cuotas:');
+
+  const montoTotal = parseFloat(montoTotalInput);
+  const cantidadCuotas = parseInt(cantidadCuotasInput);
+
+  if (isNaN(montoTotal) || isNaN(cantidadCuotas) || montoTotal <= 0 || cantidadCuotas <= 0) {
+    alert('Por favor, ingrese valores válidos para el monto total y la cantidad de cuotas.');
+    return;
+  }
+
+  const valorCuota = calcularValorCuota(montoTotal, cantidadCuotas);
+  const cuotas = new Array(cantidadCuotas).fill(valorCuota);
+
+  mostrarResultados(cuotas);
+
+  const confirmacion = confirm('¿Desea calcular cuántos meses faltan para terminar?');
+  if (confirmacion) {
+    const mesesFaltantes = cantidadCuotas;
+    alert(`Meses faltantes para terminar: ${mesesFaltantes}`);
+  }
+}
+
+calcularCuotas();
